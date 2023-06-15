f65f20b58823573f1810f692f689fc5d6985c663
babu-20 <saidbabuu7320@gmail.com>
2023-06-15 12:05:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * main - main block
+ * return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
