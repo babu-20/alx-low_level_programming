3a5d8bb6a5bd6972a0d40a85842840e702bde286
babu-20 <saidbabuu7320@gmail.com>
2023-06-15 12:22:21 -0700

    a+x 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.h> 
- /** 
-  * main - main block 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
