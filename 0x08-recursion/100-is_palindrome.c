999e94bb48470834cc8ea101f4feaa0c401b7e6a
babu-20 <saidbabuu7320@gmail.com>
2023-07-05 14:17:25 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,89 +1,50 @@
 #include "main.h"
 
-
-int check_pal(char *s, int i, int len);
-
-int _strlen_recursion(char *s);
-
-
 /**
-
- * is_palindrome - checks if a string is a palindrome
-
- * @s: string to reverse
-
+ * _strlen_recursion - Returns the length of a string.
  *
-
- * Return: 1 if it is, 0 it's not
-
- */
-
-int is_palindrome(char *s)
-
-{
-
-        if (*s == 0)
-
-                return (1);
-
-        return (check_pal(s, 0, _strlen_recursion(s)));
-
-}
-
-
-/**
-
- * _strlen_recursion - returns the length of a string
-
- * @s: string to calculate the length of
-
+ * @s: The string.
  *
-
- * Return: length of the string
-
+ * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
-
 {
-
-        if (*s == '\0')
-
-                return (0);
-
-        return (1 + _strlen_recursion(s + 1));
-
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
+/**
+ * comparator - Compares each character of the string.
+ *
+ * @s: The string.
+ * @n1: The smallest iterator.
+ * @n2: The largest iterator.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
 
 /**
-
- * check_pal - checks the characters recursively for palindrome
-
- * @s: string to check
-
- * @i: iterator
-
- * @len: length of the string
-
+ * is_palindrome - Checks if a string is a palindrome.
  *
-
- * Return: 1 if palindrome, 0 if not
-
+ * @s: The string.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-
-int check_pal(char *s, int i, int len)
-
+int is_palindrome(char *s)
 {
-
-        if (*(s + i) != *(s + len - 1))
-
-                return (0);
-
-        if (i >= len)
-
-                return (1);
-
-        return (check_pal(s, i + 1, len - 1));
-
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
