735b571049b46de80a0b1d44f9543018c9df6310
babu-20 <saidbabuu7320@gmail.com>
2023-07-17 09:59:21 -0700

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Name of structure is dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - new struct name
+ */
+typedef struct dog dog_t;
+
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
